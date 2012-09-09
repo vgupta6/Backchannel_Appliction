e89d430e5aeac3728419aa9f0368617307e9755d
vibhorgupta <vgupta6@ncsu.edu>
2012-09-09 02:31:19 -0400

    Blog deleted

--- blog/app/models/post.rb
+++
@@ -1,5 +0,0 @@
-class Post < ActiveRecord::Base
-  attr_accessible :content, :name, :title
-  validates_presence_of :name, :title
-  validates_length_of :title, :minimum => 5
-end
--- blog/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Blog::Application.config.secret_token = '810d6aa88bc86564475edfdc6569a7aff951c3fee862a5f19321982d402caddb61429880eb03da7f11948cd635f036fd7742c4ea223a81d07be338df8262c992'
